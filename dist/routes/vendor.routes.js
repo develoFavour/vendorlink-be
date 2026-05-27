e395d72bfdfcb8bfa7b6dca61d1116329fddf93f
opia favour <opiafavour10@gmail.com>
2026-05-27 08:36:27 +0100

    update: package.json

--- dist/routes/vendor.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const vendor_controller_1 = require("../controllers/vendor.controller");
-const router = (0, express_1.Router)();
-router.get("/", vendor_controller_1.getVendors);
-router.get("/:slug", vendor_controller_1.getVendorBySlug);
-exports.default = router;
