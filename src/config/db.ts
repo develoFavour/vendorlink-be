0ff62f8924664311e9a548ad5e69e8c4e950e2ab
opia favour <opiafavour10@gmail.com>
2026-05-30 08:59:05 +0100

    fix: timeout

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    const conn = await mongoose.connect(process.env.MONGO_URI as string, {
+      serverSelectionTimeoutMS: 10000,
+      connectTimeoutMS: 10000,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}/${conn.connection.name}`);
   } catch (error: any) {
     console.error(`Error: ${error.message}`);
